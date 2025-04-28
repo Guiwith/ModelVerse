c81f5932d4ef9e786bf9b6c4dd3622418d6cc668
GUIGUIzc <862121801@qq.com>
2025-04-28 16:38:06 +0800

    init

--- frontend/src/router/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'vue-router' {
-  import { RouteRecordRaw } from 'vue-router/dist/vue-router'
-  
-  export function createRouter(options: any): any
-  export function createWebHistory(): any
-  export { RouteRecordRaw }
-} 
